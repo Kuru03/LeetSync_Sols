02e4ef89e207155a6f58d6da5cddcf335d57b41e
kurush rastogi <92333395+Kuru03@users.noreply.github.com>
2024-10-25 13:51:47 +0530

    Time: 202 ms (38.74%) | Memory: 142.8 MB (21.43%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,78 @@
+class trieNode {
+public:
+    // vector<trieNode> child;
+    unordered_map<string, trieNode*> child;
+    bool end;
+    trieNode() { end = 0; }
+};
+
+class trie {
+public:
+    trieNode* root = new trieNode();
+
+    void insert(vector<string> s) {
+        trieNode* temproot = root;
+        for (int i = 0; i < s.size(); i++) {
+            // cout << "  d " << s[i] << endl;
+            if (temproot->child.count(s[i]) == 0) {
+                trieNode* temp = new trieNode();
+                temproot->child[s[i]] = temp;
+            }
+            temproot = temproot->child[s[i]];
+        }
+        temproot->end = 1;
+        return;
+    }
+    bool include(vector<string> s) {
+        trieNode* temproot = root;
+        for (int i = 0; i < s.size(); i++) {
+            if (temproot->child.count(s[i]) == 0) {
+                // cout << s[i] << endl;
+                return true;
+            }
+            // cout << s[i] << " " << temproot->child[s[i]]->end << endl;
+            if (temproot->child[s[i]]->end == 1)
+                return false;
+            temproot = temproot->child[s[i]];
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    static bool cmp(const string& a, const string& b) {
+        return a.size() < b.size();
+    }
+
+    vector<string> removeSubfolders(vector<string>& folder) {
+        vector<string> ans;
+        sort(folder.begin(), folder.end(), cmp);
+        trie root;
+
+        for (int i = 0; i < folder.size(); i++) {
+            vector<string> parse;
+            string a;
+            for (int j = 1; j < folder[i].size(); j++) {
+                if (folder[i][j] == '/') {
+                    parse.push_back(a);
+                    a = "";
+                    continue;
+                }
+                a += folder[i][j];
+            }
+            parse.push_back(a);
+
+            // for (int t = 0; t < parse.size(); t++) {
+            //     cout << parse[t] << endl;
+            // }
+            bool add = root.include(parse);
+            // cout << folder[i] << " " << add << endl;
+            if (add) {
+                root.insert(parse);
+                ans.push_back(folder[i]);
+            }
+        }
+        return ans;
+    }
+};
