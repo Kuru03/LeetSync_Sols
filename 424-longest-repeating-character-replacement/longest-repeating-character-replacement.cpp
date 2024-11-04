8ef200f46440d38e403fc3bebf28e9844bf35b58
kurush rastogi <92333395+Kuru03@users.noreply.github.com>
2024-11-05 01:00:30 +0530

    Time: 530 ms (5.01%) | Memory: 232.2 MB (5.12%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxfreq(map<char,int>charFrequencyStore){
+        int  t=0;
+        for(auto it:charFrequencyStore){
+            t=max(t,it.second);
+        }
+        return t;
+    }
+    int characterReplacement(string s, int k) {
+        int start=0,end=0;
+        int n=s.size();
+        map<char,int> charFrequencyStore;
+        int  ans=0;
+        while(end<n){
+            charFrequencyStore[s[end]]++;
+            int maxFrequency=maxfreq(charFrequencyStore);
+            int mistakes=(end-start+1)-maxFrequency;
+            while(k<mistakes){
+                charFrequencyStore[s[start]]--;
+                start++; 
+                maxFrequency=maxfreq(charFrequencyStore);
+                mistakes=(end-start+1)-maxFrequency;
+            }
+            ans=max(ans,end-start+1);
+            end++;
+        }
+        return ans;
+        
+    }
+};
