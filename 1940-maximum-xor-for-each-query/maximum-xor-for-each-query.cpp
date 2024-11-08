77e810026f16b64f9c7a36af8280ffbd9404da39
kurush rastogi <92333395+Kuru03@users.noreply.github.com>
2024-11-08 19:08:54 +0530

    Time: 3 ms (67.62%) | Memory: 98.2 MB (43.73%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -4,25 +4,13 @@ class Solution
         vector<int> getMaximumXor(vector<int> &nums, int maximumBit)
         {
             vector<int> vec = nums;
-            vector<int> ans;
             int n = vec.size();
-            for (int i = 1; i < n; i++)
+            vector<int> ans(n,0);
+            int curr=(1<<maximumBit)-1;
+            for (int i = 0; i < n; i++)
             {
-                vec[i] ^= vec[i - 1];
-            }
-            for (int i = n - 1; i >= 0; i--)
-            {
-                int maxQueryVal = 0;
-                for (int k = 0; k < maximumBit; k++)
-                {
-                   	// cout<<(1<<k)<<" "<<((1<<k) &vec[i])<<" "<<vec[i]<<endl;
-                    if (((1 << k) &vec[i]) == 0)
-                    {
-
-                        maxQueryVal |= (1 << k);
-                    }
-                }
-                ans.push_back(maxQueryVal);
+                if(i!=0)vec[i] ^= vec[i - 1];
+                ans[n-i-1] = ~vec[i] & curr;
             }
             return ans;
         }
