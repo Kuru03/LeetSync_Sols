124109eafd2ec0152f3177972d5a74350d7732c4
kurush rastogi <92333395+Kuru03@users.noreply.github.com>
2024-11-13 11:31:22 +0530

    Time: 269 ms (5.43%) | Memory: 92.3 MB (85.00%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    static bool cmp(vector<int> &a,vector<int> &b){
+        if(a[0]==b[0]){
+            return b[1]>a[1];
+        }
+        return b[0]>a[0];
+    }
+    int findMaximumBeauty(vector<vector<int>>& arr, int query) {
+        int lo = 0, hi = arr.size() - 1;
+        int ans = 0;
+        cout<<query<<endl;
+        while (lo <= hi) {
+            int mid = (hi + lo) / 2;
+            // cout<<arr[lo][0]<<" "<<arr[hi][0]<<" "<<arr[mid][0]<<endl;
+            if (arr[mid][0] > query) {
+                hi = mid - 1;
+            } else {
+                lo = mid+1;
+                ans = arr[mid][1];
+            }
+        }
+        return ans;
+    }
+
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        sort(items.begin(), items.end(),cmp);
+        for(int i=1;i<items.size();i++) items[i][1]=max(items[i][1],items[i-1][1]);
+        int n = queries.size();
+        vector<int> maximumBeautyStore;
+        for (int i = 0; i < n; i++) {
+            int maximumQueryBeauty = findMaximumBeauty(items, queries[i]);
+            maximumBeautyStore.emplace_back(maximumQueryBeauty);
+        }
+        return maximumBeautyStore;
+    }
+};
