5f0c4df38f8ee75ece4d725f80a149ff83aa34ec
kurush rastogi <92333395+Kuru03@users.noreply.github.com>
2025-01-08 23:26:25 +0530

    Time: 2915 ms (5.05%) | Memory: 670.3 MB (5.00%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,64 @@
+
+class Solution {
+public:
+    vector<int> lps(string s) {
+        int len = 0, i = 1;
+        int n = s.size();
+        vector<int> vec(n, 0);
+        while (i < n) {
+            if (s[i] == s[len]) {
+                len++;
+                vec[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = vec[len - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return vec;
+    }
+
+    vector<int> stringMatcher(string pat, string s) {
+        vector<int> vec = lps(pat);
+        vector<int> ans;
+        int i = 0, j = 0, n = s.size(), m = pat.size();
+        while (i < n) {
+            if (s[i] == pat[j]) {
+                i++;
+                j++;
+                if (j == m) {
+                    ans.push_back(i - j);
+                    j = vec[j - 1];
+                }
+            } else {
+                if (j != 0)
+                    j = vec[j - 1];
+                else
+                    i++;
+            }
+        }
+        return ans;
+    }
+
+    int repeatedStringMatch(string a, string b) {
+        int n = a.size(), m = b.size();
+        string t = a;
+        while (a.size() < m)
+            a += t;
+        int ans = 0;
+        vector<int> temp = stringMatcher(b, a);
+        if (temp.size() > 0)
+            return a.size() / t.size();
+        int p = 2 * (a.size() / t.size());
+        for (int i = 0; i <= p; i++) {
+            a += t;
+            temp = stringMatcher(b, a);
+            if (temp.size() > 0)
+                return a.size() / t.size();
+        }
+        return -1;
+    }
+};
